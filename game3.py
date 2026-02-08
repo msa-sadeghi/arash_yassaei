b281303a371285f8e4b3bc7de27bd12e48b34afc
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-02-08 19:49:36 +0330

    codes

--- game3.py
+++ game3.py
@@ -1,16 +1,13 @@
 import pygame
 from player import Player
 from coin  import Coin
+from ground import Ground
 pygame.init()
-
 WIDTH = 1000
 HEIGHT =  640
-
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 player1 = Player(100, 100, 10)
 coin_image = pygame.image.load("coin.png")
-
-
 coin_group = pygame.sprite.Group()
 coin1 = Coin(coin_image, 450, 480, 5, "move_x")
 coin2 = Coin(coin_image, 750, 280, 5, "move_y")
@@ -18,11 +15,10 @@ coin_group.add(coin1,coin2)
 clicked = False
 FPS = 60
 CLOCK = pygame.time.Clock()
-
 score = 0
 myfont = pygame.font.Font("f.otf", 24)
 score_text = myfont.render(f"Score: {score}", True, "darkblue")
-
+ground = Ground(WIDTH, HEIGHT)
 circle_color = "red"
 running = True
 while running:
@@ -47,4 +43,6 @@ while running:
     player1.jump()
     for coin in coin_group.sprites():
         score = coin.check_collision(player1, score)
+
+    ground.draw(screen)
     pygame.display.update()
--- ground.py
+++ ground.py
@@ -1,5 +1,8 @@
 import pygame
 
 class Ground:
-    def __init__(self):
-        pass
+    def __init__(self, width, height):
+        self.rect = pygame.Rect(0, height - 40, width, 40)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen,"green", self.rect)
